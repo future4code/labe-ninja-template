12f87bbe39d534fe8a146652e56fc841e7f11880
Soter Padua <soter19@gmail.com>
2020-08-30 21:49:19 -0300

    updating dependencies

--- src/App.js
+++ src/App.js
@@ -6,29 +6,11 @@
 //
 
 import React from 'react'
-import JssProvider from 'react-jss/lib/JssProvider'
-import { create } from 'jss'
-import { MuiThemeProvider, createGenerateClassName, jssPreset } from '@material-ui/core/styles'
-import { createMuiTheme } from '@material-ui/core/styles'
-import CssBaseline from '@material-ui/core/CssBaseline'
 import { AppContainer } from './components/AppContainer'
-
-const generateClassName = createGenerateClassName()
-const jss = create({
-	...jssPreset(),
-	insertionPoint: document.getElementById('jss-insertion-point'),
-})
-
-const theme = createMuiTheme()
 
 function App() {
 	return (
-		<JssProvider jss={jss} generateClassName={generateClassName}>
-			<MuiThemeProvider theme={theme}>
-				<CssBaseline />
-				<AppContainer />
-			</MuiThemeProvider>
-		</JssProvider>
+        <AppContainer />
 	)
 }
 
